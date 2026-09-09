6e853f50fd54b8247e20c7e39f4aaa46a173433e
bsl-isaacmrongo <isaac.mrongo@braeburn.ac.ke>
2026-09-09 17:50:27 +0300

    updates

--- src/app/env.prod.ts
+++ src/app/env.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   username: 'charles.mutinda@braeburn.ac.ke',
   password: 'fniDF3CGu%Ytj%0sK2zu',
-  apiUrl: 'https://bnisapi.braeburn.ac.ke/api',
+  apiUrl: '/api',
 };
